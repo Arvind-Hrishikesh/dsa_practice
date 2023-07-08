7428e39e419d93d868fb2021eb67c0a707614d9f
Arvind Hrishikesh <ht2566@columbia.edu>
2023-07-07 23:50:32 -0400

    cleaned subarray given sum

--- subarray_given_sum_linear_time.py
+++ subarray_given_sum_linear_time.py
@@ -6,20 +6,18 @@ class Solution:
         end = 1
         sub_array = arr[0]
         while begin<=end and end <=n:
-            #sub_array = arr[begin:end]
             if sub_array < s:
                 try:
                     sub_array += arr[end]
-                    end+=1
                 except:
-                    end+=1
                     pass
+                end+=1
             elif sub_array > s:
                 sub_array -= arr[begin]
                 begin+=1
             else:
                 return begin+1,end
-        return [-1]          
+        return [-1]         
         
 
 print(Solution().subArraySum([1,2,3,4],4,0))
